e17998ee79e8cd8722176a39e6b0ef5ea8999f25
Thomas Metois <thomas@netlor.fr>
2016-11-21 09:05:15 +0100

    FIX User roles controller extends AbstractController

--- src/MonarcFO/Controller/ApiAdminUsersRolesControllerFactory.php
+++ src/MonarcFO/Controller/ApiAdminUsersRolesControllerFactory.php
@@ -2,7 +2,7 @@
 
 namespace MonarcFO\Controller;
 
-class ApiAdminUsersRolesControllerFactory extends \MonarcCore\Controller\AbstractControllerFactory
+class ApiAdminUsersRolesControllerFactory extends \MonarcCore\Controller\AbstractController
 {
     protected $serviceName = '\MonarcFO\Service\UserRoleService';
 }
