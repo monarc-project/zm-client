7869ad87650402c58394b1bef159e8bfb7f2ae40
Thomas Metois <thomas@netlor.fr>
2016-11-22 11:47:37 +0100

    ANR: asset import (from file)

--- src/MonarcFO/Service/AnrAssetServiceFactory.php
+++ src/MonarcFO/Service/AnrAssetServiceFactory.php
@@ -6,8 +6,19 @@ use MonarcCore\Service\AbstractServiceFactory;
 class AnrAssetServiceFactory extends AbstractServiceFactory
 {
     protected $ressources = array(
-        'entity'=> 'MonarcFO\Model\Entity\Asset',
-        'table'=> 'MonarcFO\Model\Table\AssetTable',
+        'entity' => 'MonarcFO\Model\Entity\Asset',
+        'table' => 'MonarcFO\Model\Table\AssetTable',
         'anrTable' => 'MonarcFO\Model\Table\AnrTable',
+
+        'amvTable' => 'MonarcFO\Model\Table\AmvTable',
+		'amvEntity' => 'MonarcFO\Model\Entity\Amv',
+		'threatTable' => 'MonarcFO\Model\Table\ThreatTable',
+		'threatEntity' => 'MonarcFO\Model\Entity\Threat',
+		'themeTable' => 'MonarcFO\Model\Table\ThemeTable',
+		'themeEntity' => 'MonarcFO\Model\Entity\Theme',
+		'vulnerabilityTable' => 'MonarcFO\Model\Table\VulnerabilityTable',
+		'vulnerabilityEntity' => 'MonarcFO\Model\Entity\Vulnerability',
+		'measureTable' => 'MonarcFO\Model\Table\MeasureTable',
+		'measureEntity' => 'MonarcFO\Model\Entity\Measure',
     );
 }
