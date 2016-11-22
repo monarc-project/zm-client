fb54df52ca563bd68c1143f1d52afe25aaffb4b2
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-11-22 09:42:18 +0100

    manage rights to an anr for a user

--- src/MonarcFO/Service/UserServiceFactory.php
+++ src/MonarcFO/Service/UserServiceFactory.php
@@ -7,8 +7,10 @@ class UserServiceFactory extends AbstractServiceFactory
 {
     protected $ressources = array(
         'table'=> '\MonarcFO\Model\Table\UserTable',
+        'userAnrTable'=> '\MonarcFO\Model\Table\UserAnrTable',
         'userRoleTable'=> '\MonarcFO\Model\Table\UserRoleTable',
         'entity'=> '\MonarcFO\Model\Entity\User',
+        'userAnrService' => 'MonarcFO\Service\UserAnrService',
         'userRoleService' => 'MonarcFO\Service\UserRoleService',
     );
 }
