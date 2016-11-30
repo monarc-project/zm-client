c0d5a0b5c9fc1b44d27d18be2d509d0f51abcdf3
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-11-30 14:46:52 +0100

    delete and update snapshot

--- src/MonarcFO/Service/SnapshotService.php
+++ src/MonarcFO/Service/SnapshotService.php
@@ -2,6 +2,7 @@
 namespace MonarcFO\Service;
 
 use MonarcFO\Model\Table\AnrTable;
+use MonarcFO\Model\Table\SnapshotTable;
 use MonarcFO\Service\AbstractService;
 
 /**
@@ -16,6 +17,13 @@ class SnapshotService extends \MonarcCore\Service\AbstractService
     protected $anrTable;
     protected $anrService;
 
+    /**
+     * Create
+     *
+     * @param $data
+     * @param bool $last
+     * @return mixed
+     */
     public function create($data, $last = true) {
 
         $anrReferenceId = $data['anr'];
@@ -33,5 +41,51 @@ class SnapshotService extends \MonarcCore\Service\AbstractService
         $data['anr'] = $anrId;
 
         return parent::create($data);
+    }
+
+    /**
+     * Patch
+     *
+     * @param $id
+     * @param $data
+     * @return mixed
+     */
+    public function patch($id, $data){
+
+        foreach($data as $key => $value) {
+            if ($key != 'comment') {
+                unset($data[$key]);
+            }
+        }
+
+        return parent::patch($id, $data);
+    }
+
+    /**
+     * Update
+     *
+     * @param $id
+     * @param $data
+     * @return mixed
+     */
+    public function update($id, $data) {
+        return $this->patch($id, $data);
+    }
+
+    /**
+     * Delete
+     *
+     * @param $id
+     */
+    public function delete($id) {
+
+        /** @var SnapshotTable $snapshotTable */
+        $snapshotTable = $this->get('table');
+        $snapshot = $snapshotTable->getEntity($id);
+
+        /** @var AnrService $anrService */
+        $anrService = $this->get('anrService');
+
+        return $anrService->delete($snapshot->anr->id);
     }
 }
