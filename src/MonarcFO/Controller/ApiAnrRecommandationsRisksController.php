e9d02adf98688a79069bccd29782a64118d8f81c
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-12-13 09:49:28 +0100

    filter recommandations-risks by risk

--- src/MonarcFO/Controller/ApiAnrRecommandationsRisksController.php
+++ src/MonarcFO/Controller/ApiAnrRecommandationsRisksController.php
@@ -15,6 +15,53 @@ class ApiAnrRecommandationsRisksController extends ApiAnrAbstractController
     protected $name = 'recommandations-risks';
     protected $dependencies = ['anr', 'recommandation'];
 
+    /**
+     * Get List
+     *
+     * @return JsonModel
+     * @throws \Exception
+     */
+    public function getList()
+    {
+        $page = $this->params()->fromQuery('page');
+        $limit = $this->params()->fromQuery('limit');
+        $order = $this->params()->fromQuery('order');
+        $filter = $this->params()->fromQuery('filter');
+        $status = $this->params()->fromQuery('status');
+        $risk = $this->params()->fromQuery('risk');
+        $op = $this->params()->fromQuery('op');
+
+        $anrId = (int) $this->params()->fromRoute('anrid');
+        if(empty($anrId)){
+            throw new \Exception('Anr id missing', 412);
+        }
+
+        $filterAnd = ['anr' => $anrId];
+
+        if (!is_null($status) && $status != 'all') {
+            $filterAnd['status'] = $status;
+        }
+
+        if (!is_null($risk)) {
+            $fieldName = ($op) ? 'instanceRiskOp' : 'instanceRisk';
+            $filterAnd[$fieldName] = intval($risk);
+        }
+
+        $service = $this->getService();
+
+        $entities = $service->getList($page, $limit, $order, $filter, $filterAnd);
+        if (count($this->dependencies)) {
+            foreach ($entities as $key => $entity) {
+                $this->formatDependencies($entities[$key], $this->dependencies);
+            }
+        }
+
+        return new JsonModel(array(
+            'count' => $service->getFilteredCount($page, $limit, $order, $filter, $filterAnd),
+            $this->name => $entities
+        ));
+    }
+
     public function update($id, $data)
     {
         return $this->methodNotAllowed();
