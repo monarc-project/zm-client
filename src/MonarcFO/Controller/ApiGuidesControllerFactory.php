d3a3bbb0180072ae246d384299394cd13bd58c85
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-12-16 09:24:29 +0100

    add api guides

---
+++ src/MonarcFO/Controller/ApiGuidesControllerFactory.php
@@ -0,0 +1,8 @@
+<?php
+namespace MonarcFO\Controller;
+
+class ApiGuidesControllerFactory extends \MonarcCore\Controller\AbstractControllerFactory
+{
+    protected $serviceName = '\MonarcCore\Service\GuideService';
+}
+
