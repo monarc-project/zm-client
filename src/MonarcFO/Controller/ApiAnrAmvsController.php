17a419011dcb06468433370653d04054792505c8
Guillaume Lesniak <guillaume@netlor.fr>
2016-12-17 02:26:28 +0100

    AMVs: Calculate implicitPosition

--- src/MonarcFO/Controller/ApiAnrAmvsController.php
+++ src/MonarcFO/Controller/ApiAnrAmvsController.php
@@ -14,4 +14,52 @@ class ApiAnrAmvsController extends ApiAnrAbstractController
 {
     protected $name = 'amvs';
     protected $dependencies = ['asset', 'threat', 'vulnerability', 'measure1', 'measure2', 'measure3'];
+
+
+    /**
+     * Get
+     *
+     * @param mixed $id
+     * @return JsonModel
+     */
+    public function get($id)
+    {
+        $entity = $this->getService()->getEntity($id);
+
+        if (count($this->dependencies)) {
+            $this->formatDependencies($entity, $this->dependencies);
+        }
+
+        // Find out the entity's implicitPosition and previous
+        if ($entity['position'] == 1) {
+            $entity['implicitPosition'] = 1;
+        } else {
+            // We're not at the beginning, get all AMV links of the same asset, and figure out position and previous
+            $amvsAsset = $this->getService()->getList(1, 0, 'position', null, ['asset' => $entity['asset']['id']]);
+
+            $i = 0;
+            foreach ($amvsAsset as $amv) {
+                if ($amv['id'] == $entity['id']) {
+                    if ($i == count($amvsAsset) - 1) {
+                        $entity['implicitPosition'] = 2;
+                    } else {
+                        if ($i == 0) {
+                            $entity['implicitPosition'] = 1;
+                            $entity['previous'] = null;
+                        } else {
+                            $entity['implicitPosition'] = 3;
+                            $entity['previous'] = $amvsAsset[$i - 1];
+                            $this->formatDependencies($entity['previous'], $this->dependencies);
+                        }
+                    }
+
+                    break;
+                }
+
+                ++$i;
+            }
+        }
+
+        return new JsonModel($entity);
+    }
 }
