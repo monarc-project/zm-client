cf365dfd9f2bfdc6a37c966aa94bbe40a7c1e615
Guillaume Lesniak <guillaume@netlor.fr>
2016-12-19 11:03:08 +0100

    AnrLibrary: Properly sort result by position

--- src/MonarcFO/Controller/ApiAnrLibraryController.php
+++ src/MonarcFO/Controller/ApiAnrLibraryController.php
@@ -27,9 +27,31 @@ class ApiAnrLibraryController extends \MonarcCore\Controller\AbstractController
         $fields = ['id', 'label1', 'label2', 'label3', 'label4', 'position', 'objects', 'child'];
         $objectsCategories = $this->recursiveArray($objectsCategories, null, 0, $fields);
 
+        usort($objectsCategories, function ($a, $b) { return $this->sortCategories($a, $b); });
+        foreach ($objectsCategories as &$cat) {
+            if (isset($cat['child']) && is_array($cat['child'])) {
+                usort($cat['child'], function ($a, $b) {
+                    return $this->sortCategories($a, $b);
+                });
+            }
+        }
+
         return new JsonModel(array(
             $this->name => $objectsCategories
         ));
+    }
+
+    private function sortCategories($a, $b) {
+        if (isset($a['position']) && isset($b['position'])) {
+            //echo "all set for " . $a['label1'] . ' and ' . $b['label1'];
+            return ($a['position'] - $b['position']);
+        } else if (isset($a['position']) && !isset($b['position'])) {
+            return -1;
+        } else if (isset($b['position']) && !isset($a['position'])) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
     public function get($id)
