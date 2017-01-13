5dcdd1d5a275ff1db797975f51a90aaf29499ca2
Thomas Metois <thomas@netlor.fr>
2017-01-13 09:57:39 +0100

    Revert "fix bug order categories in library"
    This reverts commit 7320eb245bf59707b303ca4e996d6daeb0a1a74c.

--- src/MonarcFO/Controller/ApiAnrLibraryController.php
+++ src/MonarcFO/Controller/ApiAnrLibraryController.php
@@ -27,7 +27,6 @@ class ApiAnrLibraryController extends \MonarcCore\Controller\AbstractController
         $fields = ['id', 'label1', 'label2', 'label3', 'label4', 'position', 'objects', 'child'];
         $objectsCategories = $this->recursiveArray($objectsCategories, null, 0, $fields);
 
-        /*
         usort($objectsCategories, function ($a, $b) { return $this->sortCategories($a, $b); });
         foreach ($objectsCategories as &$cat) {
             if (isset($cat['child']) && is_array($cat['child'])) {
@@ -36,7 +35,6 @@ class ApiAnrLibraryController extends \MonarcCore\Controller\AbstractController
                 });
             }
         }
-        */
 
         return new JsonModel(array(
             $this->name => $objectsCategories
