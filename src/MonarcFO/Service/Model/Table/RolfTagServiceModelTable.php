767a92f2c26085626143f24e8bff2a9d3670e669
Jerome De Almeida <jerome.dealmeida@vesperiagroup.com>
2017-01-31 15:59:43 +0100

    add license information in files

--- src/MonarcFO/Service/Model/Table/RolfTagServiceModelTable.php
+++ src/MonarcFO/Service/Model/Table/RolfTagServiceModelTable.php
@@ -1,8 +1,18 @@
 <?php
+/**
+ * @link      https://github.com/CASES-LU for the canonical source repository
+ * @copyright Copyright (c) Cases is a registered trademark of SECURITYMADEIN.LU
+ * @license   MyCases is licensed under the GNU Affero GPL v3 - See license.txt for more information
+ */
+
 namespace MonarcFO\Service\Model\Table;
 
 use MonarcCore\Service\Model\Table\AbstractServiceModelTable;
 
+/**
+ * Class RolfTagServiceModelTable
+ * @package MonarcFO\Service\Model\Table
+ */
 class RolfTagServiceModelTable extends AbstractServiceModelTable
 {
     protected $dbService = '\MonarcCli\Model\Db';
