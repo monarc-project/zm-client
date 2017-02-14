a327ede96f1e6a3dd7924f81470c754d0c98dda0
Thomas Metois <thomas@netlor.fr>
2017-02-14 09:40:38 +0100

    Anr > risk(op) & recommandations detach

--- src/MonarcFO/Service/AnrRecommandationRiskService.php
+++ src/MonarcFO/Service/AnrRecommandationRiskService.php
@@ -654,6 +654,7 @@ class AnrRecommandationRiskService extends \MonarcCore\Service\AbstractService
 
         $idAnr = $recommandationRisk->anr->id;
         $idReco = $recommandationRisk->recommandation->id;
+        $pos = $recommandationRisk->recommandation->position;
 
         if ($recommandationRisk->instanceRisk) {
             /** @var InstanceRiskTable $instanceRiskTable */
@@ -705,7 +706,7 @@ class AnrRecommandationRiskService extends \MonarcCore\Service\AbstractService
 
         // Update brother's recommandation position if necessary
         $bros = current($table->getEntityByFields(['anr' => $idAnr,'recommandation'=>$idReco, 'id'=>['op'=>'!=', 'value'=>$id]]));
-        if(empty($bros) && $reco->get('position') > 0){ // is last recorisk
+        if(empty($bros) && $pos > 0){ // is last recorisk
             $reco = $this->get('recommandationTable')->getEntity($idReco);
             $recos = $this->get('recommandationTable')->getEntityByFields(['anr'=>$reco->get('anr')->get('id'), 'position' => ['op' => '>', 'value'=>$reco->get('position')]],['position'=>'ASC']);
             foreach($recos as $r){
