6c5e4dfda23d6cd0ab3e5f54a45f9c3f5c59c258
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-04-04 11:57:27 +0200

    Add a boolean attribute 'editable' for the deliveries templates.

--- migrations/db/20180322092327_add_table_deliveries_models.php
+++ migrations/db/20180322092327_add_table_deliveries_models.php
@@ -32,14 +32,15 @@ class AddTableDeliveriesModels extends AbstractMigration
         $table
             ->addColumn('anr_id', 'integer', array('null' => true, 'signed' => false))
             ->addColumn('category', 'integer', array('default' => '0', 'limit' => MysqlAdapter::INT_TINY))
-            ->addColumn('description1', 'string', array('default' => '', 'limit' => 255))
-            ->addColumn('description2', 'string', array('default' => '', 'limit' => 255))
-            ->addColumn('description3', 'string', array('default' => '', 'limit' => 255))
-            ->addColumn('description4', 'string', array('default' => '', 'limit' => 255))
+            ->addColumn('description1', 'string', array('null' => true, 'default' => '', 'limit' => 255))
+            ->addColumn('description2', 'string', array('null' => true, 'default' => '', 'limit' => 255))
+            ->addColumn('description3', 'string', array('null' => true, 'default' => '', 'limit' => 255))
+            ->addColumn('description4', 'string', array('null' => true, 'default' => '', 'limit' => 255))
             ->addColumn('path1', 'string', array('null' => true, 'limit' => 255))
             ->addColumn('path2', 'string', array('null' => true, 'limit' => 255))
             ->addColumn('path3', 'string', array('null' => true, 'limit' => 255))
             ->addColumn('path4', 'string', array('null' => true, 'limit' => 255))
+            ->addColumn('editable', 'boolean', array('default' => true))
             ->addColumn('creator', 'string', array('null' => true, 'limit' => 255))
             ->addColumn('created_at', 'datetime', array('null' => true))
             ->addColumn('updater', 'string', array('null' => true, 'limit' => 255))
@@ -49,6 +50,6 @@ class AddTableDeliveriesModels extends AbstractMigration
             ->create();
         $table->changeColumn('id', 'integer',array('identity'=>true,'signed'=>false))->update();
 
-        $this->query('INSERT INTO deliveries_models (category, description1, path1, creator, created_at, updater, updated_at, description2, description3, description4, path2, path3, path4) VALUES (1, "Validation de contexte", "./deliveries/cases/FR/1.docx", NULL , NULL, NULL, NULL, "Context validation", "Kontextüberprüfung", NULL, "./deliveries/cases/EN/1.docx", "./deliveries/cases/DE/1.docx", "./deliveries/cases/NE/1.docx"), (2, "Validation du modèle"  , "./deliveries/cases/FR/2.docx", NULL , NULL, NULL, NULL, "Modelling validation", "Modellierungsüberprüfung", NULL, "./deliveries/cases/EN/2.docx", "./deliveries/cases/DE/2.docx", "./deliveries/cases/NE/2.docx"), (3, "Rapport final", "./deliveries/cases/FR/3.docx", NULL, NULL, NULL, NULL, "Report risk assessment", "Risikobeurteilungsbericht", NULL, "./deliveries/cases/EN/3.docx", "./deliveries/cases/DE/3.docx", "./deliveries/cases/NE/3.docx"), (4, "Plan dimplémentation" , "./deliveries/cases/FR/4.docx", NULL, NULL, NULL, NULL, "Implementation plan", "Implementierungsplan", "Implementatieplan", "./deliveries/cases/EN/4.docx", "./deliveries/cases/DE/4.docx", "./deliveries/cases/NE/4.docx");');
+        $this->query('INSERT INTO deliveries_models (category, description1, path1, creator, created_at, updater, updated_at, description2, description3, description4, path2, path3, path4, editable) VALUES (1, "Validation de contexte", "./deliveries/cases/FR/1.docx", NULL , NULL, NULL, NULL, "Context validation", "Kontextüberprüfung", NULL, "./deliveries/cases/EN/1.docx", "./deliveries/cases/DE/1.docx", "./deliveries/cases/NE/1.docx", false), (2, "Validation du modèle"  , "./deliveries/cases/FR/2.docx", NULL , NULL, NULL, NULL, "Modelling validation", "Modellierungsüberprüfung", NULL, "./deliveries/cases/EN/2.docx", "./deliveries/cases/DE/2.docx", "./deliveries/cases/NE/2.docx", false), (3, "Rapport final", "./deliveries/cases/FR/3.docx", NULL, NULL, NULL, NULL, "Report risk assessment", "Risikobeurteilungsbericht", NULL, "./deliveries/cases/EN/3.docx", "./deliveries/cases/DE/3.docx", "./deliveries/cases/NE/3.docx", false), (4, "Plan dimplémentation" , "./deliveries/cases/FR/4.docx", NULL, NULL, NULL, NULL, "Implementation plan", "Implementierungsplan", "Implementatieplan", "./deliveries/cases/EN/4.docx", "./deliveries/cases/DE/4.docx", "./deliveries/cases/NE/4.docx", false);');
     }
 }
