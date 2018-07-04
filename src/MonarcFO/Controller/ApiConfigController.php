decfac7d9bbc2a4bbbad773e669e8eeb44c386d7
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-07-04 14:50:34 +0200

    Added the checkVersion boolean in the config controller for the frontoffice. Related to monarc-project/MonarcAppFO#106

--- src/MonarcFO/Controller/ApiConfigController.php
+++ src/MonarcFO/Controller/ApiConfigController.php
@@ -25,7 +25,8 @@ class ApiConfigController extends AbstractController
     {
         return new JsonModel(array_merge(
                                 $this->getService()->getLanguage(),
-                                $this->getService()->getAppVersion())
+                                $this->getService()->getAppVersion(),
+                                $this->getService()->getCheckVersion())
                             );
     }
 
