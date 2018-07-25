a7414253c8e13cd60d33f2e4146c277437c2ba29
Juan Rocha <juan.rocha@securitymadein.lu>
2018-07-25 15:29:14 +0200

    Add parse parms fonction

--- src/MonarcFO/Controller/ApiAnrInstancesController.php
+++ src/MonarcFO/Controller/ApiAnrInstancesController.php
@@ -75,6 +75,7 @@ class ApiAnrInstancesController extends ApiAnrAbstractController
         /** @var InstanceService $service */
         $service = $this->getService();
         $entity = $service->getEntityByIdAndAnr($id, $anrId);
+        $params = $this->parseParams();
         if ($this->params()->fromQuery('csv', false)) {
             header('Content-Type: text/csv');
             die($this->getService()->getCsvRisksOp($anrId, $entity, $params));
@@ -122,4 +123,30 @@ class ApiAnrInstancesController extends ApiAnrAbstractController
             'id' => $id,
         ]);
     }
+
+    /**
+     * Helper function to parse query parameters
+     * @return array The sorted parameters
+     */
+    protected function parseParams()
+    {
+        $keywords = trim($this->params()->fromQuery("keywords", ''));
+        $kindOfMeasure = $this->params()->fromQuery("kindOfMeasure");
+        $order = $this->params()->fromQuery("order", "maxRisk");
+        $order_direction = $this->params()->fromQuery("order_direction", "desc");
+        $thresholds = $this->params()->fromQuery("thresholds");
+        $page = $this->params()->fromQuery("page", 1);
+        $limit = $this->params()->fromQuery("limit", 50);
+
+        return [
+            'keywords' => $keywords,
+            'kindOfMeasure' => $kindOfMeasure,
+            'order' => $order,
+            'order_direction' => $order_direction,
+            'thresholds' => $thresholds,
+            'page' => $page,
+            'limit' => $limit
+        ];
+    }
+
 }
