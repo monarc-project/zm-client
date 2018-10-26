326222f7252e71b9abc26b2a431a2a22efb30908
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-10-26 11:32:59 +0200

    Cleaning SOA related entities.

--- src/MonarcFO/Model/Entity/SoaCategory.php
+++ src/MonarcFO/Model/Entity/SoaCategory.php
@@ -1,56 +1,50 @@
 <?php
 /**
- * @link      https://github.com/monarc-project for the canonical source repository
- * @copyright Copyright (c) 2018 SMILE GIE Securitymadein.lu - Licensed under GNU Affero GPL v3
- * @license   MONARC is licensed under GNU Affero General Public License version 3
- */
+* @link      https://github.com/monarc-project for the canonical source repository
+* @copyright Copyright (c) 2018 SMILE GIE Securitymadein.lu - Licensed under GNU Affero GPL v3
+* @license   MONARC is licensed under GNU Affero General Public License version 3
+*/
 
 namespace MonarcFO\Model\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use MonarcCore\Model\Entity\SoaCategorySuperClass;
 
-
 /**
- * Category
- *
- * @ORM\Table(name="soacategory", indexes={
- *      @ORM\Index(name="anr", columns={"anr_id"})
+* Category
+*
+* @ORM\Table(name="soacategory", indexes={
+*      @ORM\Index(name="anr", columns={"anr_id"})
 * })
- * @ORM\Entity
- */
+* @ORM\Entity
+*/
 class SoaCategory extends SoaCategorySuperClass
 {
+    /**
+    * @var \MonarcFO\Model\Entity\Anr
+    *
+    * @ORM\ManyToOne(targetEntity="MonarcFO\Model\Entity\Anr", cascade={"persist"})
+    * @ORM\JoinColumns({
+    *   @ORM\JoinColumn(name="anr_id", referencedColumnName="id", nullable=true)
+    * })
+    */
+    protected $anr;
 
-  /**
-   * @var \MonarcFO\Model\Entity\Anr
-   *
-   * @ORM\ManyToOne(targetEntity="MonarcFO\Model\Entity\Anr", cascade={"persist"})
-   * @ORM\JoinColumns({
-   *   @ORM\JoinColumn(name="anr_id", referencedColumnName="id", nullable=true)
-   * })
-   */
-  protected $anr;
+    /**
+    * @return Anr
+    */
+    public function getAnr()
+    {
+        return $this->anr;
+    }
 
-
-
-
-  /**
-   * @return Anr
-   */
-  public function getAnr()
-  {
-      return $this->anr;
-  }
-
-  /**
-   * @param Anr $anr
-   * @return Category
-   */
-  public function setAnr($anr)
-  {
-      $this->anr = $anr;
-      return $this;
-  }
-
+    /**
+    * @param Anr $anr
+    * @return Category
+    */
+    public function setAnr($anr)
+    {
+        $this->anr = $anr;
+        return $this;
+    }
 }
