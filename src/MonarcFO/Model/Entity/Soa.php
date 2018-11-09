5f56c684c2c6c179c373b4dc632167b8b558a7d7
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-11-09 13:50:11 +0100

    Fixed some conflicts in Model/Entity/Soa.php

--- src/MonarcFO/Model/Entity/Soa.php
+++ src/MonarcFO/Model/Entity/Soa.php
@@ -1,9 +1,9 @@
 <?php
 /**
-* @link      https://github.com/monarc-project for the canonical source repository
-* @copyright Copyright (c) 2018 SMILE GIE Securitymadein.lu - Licensed under GNU Affero GPL v3
-* @license   MONARC is licensed under GNU Affero General Public License version 3
-*/
+ * @link      https://github.com/monarc-project for the canonical source repository
+ * @copyright Copyright (c) 2018 SMILE GIE Securitymadein.lu - Licensed under GNU Affero GPL v3
+ * @license   MONARC is licensed under GNU Affero General Public License version 3
+ */
 
 namespace MonarcFO\Model\Entity;
 
@@ -13,146 +13,156 @@ use MonarcCore\Model\Entity\AbstractEntity;
 
 
 /**
-* Soa
-*
-* @ORM\Table(name="soa", indexes={
-*      @ORM\Index(name="measure", columns={"measure_id"}),
-*      @ORM\Index(name="anr", columns={"anr_id"})
+ * Soa
+ *
+ * @ORM\Table(name="soa", indexes={
+ *      @ORM\Index(name="measure", columns={"measure_id"}),
+ *      @ORM\Index(name="anr", columns={"anr_id"})
 
 
-* })
-* @ORM\Entity
-*/
+ * })
+ * @ORM\Entity
+ */
 class Soa extends AbstractEntity
 {
 
-    /**
-    * @var integer
-    *
-    * @ORM\Column(name="id", type="integer", nullable=false)
-    * @ORM\Id
-    * @ORM\GeneratedValue(strategy="IDENTITY")
-    */
-    protected $id;
+
 
     /**
-    * @var \MonarcFO\Model\Entity\Anr
-    *
-    * @ORM\ManyToOne(targetEntity="MonarcFO\Model\Entity\Anr", cascade={"persist"})
-    * @ORM\JoinColumns({
-    *   @ORM\JoinColumn(name="anr_id", referencedColumnName="id", nullable=true)
-    * })
-    */
-    protected $anr;
-    
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+
+
+
+
+
     /**
-    * @var \MonarcFO\Model\Entity\Measure
-    * @ORM\ManyToOne(targetEntity="MonarcFO\Model\Entity\Measure", cascade={"persist"})
-    * @ORM\JoinColumns({
-    *   @ORM\JoinColumn(name="measure_id", referencedColumnName="id", nullable=true)
-    * })
-    *
-    */
+     * @var \MonarcFO\Model\Entity\Anr
+     *
+     * @ORM\ManyToOne(targetEntity="MonarcFO\Model\Entity\Anr", cascade={"persist"})
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="anr_id", referencedColumnName="id", nullable=true)
+     * })
+     */
+    protected $anr;
+
+
+
+
+    /**
+     * @var \MonarcFO\Model\Entity\Measure
+     * @ORM\ManyToOne(targetEntity="MonarcFO\Model\Entity\Measure", cascade={"persist"})
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="measure_id", referencedColumnName="id", nullable=true)
+     * })
+     *
+     */
     protected $measure;
 
 
 
 
 
-    /**
+   /**
     * @var text
     *
     * @ORM\Column(name="justification", type="text", length=255, nullable=true)
     */
-    protected $remarks ;
+  protected $remarks ;
+
+
+
+  /**
+   * @var text
+   *
+   * @ORM\Column(name="evidences", type="text", length=255, nullable=true)
+  */
+protected $evidences ;
+
+/**
+ * @var text
+ *
+ * @ORM\Column(name="actions", type="text", length=255, nullable=true)
+*/
+protected $actions ;
+
+/**
+ * @var string
+ *
+ * @ORM\Column(name="compliance", type="integer",  nullable=true)
+*/
+protected $compliance ;
+
+
+
+/**
+ * @var smallint
+*
+* @ORM\Column(name="EX", type="smallint", options={"unsigned":true, "default":0})
+ */
+protected $EX = '0';
+
+/**
+ * @var smallint
+*
+* @ORM\Column(name="LR", type="smallint", options={"unsigned":true, "default":0})
+ */
+protected $LR= '0';
+
+/**
+ * @var smallint
+*
+* @ORM\Column(name="CO", type="smallint", options={"unsigned":true, "default":0})
+ */
+protected $CO = '0';
+
+/**
+ * @var smallint
+*
+* @ORM\Column(name="BR", type="smallint", options={"unsigned":true, "default":0})
+ */
+protected $BR = '0';
+
+/**
+ * @var smallint
+*
+* @ORM\Column(name="BP", type="smallint", options={"unsigned":true, "default":0})
+ */
+protected $BP = '0';
+
+
+/**
+ * @var smallint
+*
+* @ORM\Column(name="RRA", type="smallint", options={"unsigned":true, "default":0})
+ */
+protected $RRA = '0';
+
+
+
+
+
 
 
 
     /**
-    * @var text
-    *
-    * @ORM\Column(name="evidences", type="text", length=255, nullable=true)
-    */
-    protected $evidences ;
-
-    /**
-    * @var text
-    *
-    * @ORM\Column(name="actions", type="text", length=255, nullable=true)
-    */
-    protected $actions ;
-
-    /**
-    * @var string
-    *
-    * @ORM\Column(name="compliance", type="integer",  nullable=true)
-    */
-    protected $compliance ;
-
-
-
-    /**
-    * @var smallint
-    *
-    * @ORM\Column(name="EX", type="smallint", options={"unsigned":true, "default":0})
-    */
-    protected $EX = '0';
-
-    /**
-    * @var smallint
-    *
-    * @ORM\Column(name="LR", type="smallint", options={"unsigned":true, "default":0})
-    */
-    protected $LR= '0';
-
-    /**
-    * @var smallint
-    *
-    * @ORM\Column(name="CO", type="smallint", options={"unsigned":true, "default":0})
-    */
-    protected $CO = '0';
-
-    /**
-    * @var smallint
-    *
-    * @ORM\Column(name="BR", type="smallint", options={"unsigned":true, "default":0})
-    */
-    protected $BR = '0';
-
-    /**
-    * @var smallint
-    *
-    * @ORM\Column(name="BP", type="smallint", options={"unsigned":true, "default":0})
-    */
-    protected $BP = '0';
-
-
-    /**
-    * @var smallint
-    *
-    * @ORM\Column(name="RRA", type="smallint", options={"unsigned":true, "default":0})
-    */
-    protected $RRA = '0';
-
-
-
-
-
-
-
-
-    /**
-    * @return int
-    */
+     * @return int
+     */
     public function getId()
     {
         return $this->id;
     }
 
     /**
-    * @param int $id
-    *
-    */
+     * @param int $id
+     *
+     */
     public function setId($id)
     {
         $this->id = $id;
@@ -160,16 +170,16 @@ class Soa extends AbstractEntity
 
     /**
     * @return Measure     *
-    */
+     */
     public function getMeasure()
     {
         return $this->measure;
     }
 
     /**
-    * @param Measure  $measure
-    *
-    */
+     * @param Measure  $measure
+     *
+     */
     public function setMeasure($measure)
     {
         $this->measure = $measure;
@@ -179,17 +189,17 @@ class Soa extends AbstractEntity
 
 
     /**
-    * @return Anr
-    */
+     * @return Anr
+     */
     public function getAnr()
     {
         return $this->anr;
     }
 
     /**
-    * @param Anr $anr
-    * @return Soa
-    */
+     * @param Anr $anr
+     * @return Soa
+     */
     public function setAnr($anr)
     {
         $this->anr = $anr;
@@ -203,17 +213,17 @@ class Soa extends AbstractEntity
 
 
     /**
-    * @return TEXT_LONG
-    */
+     * @return TEXT_LONG
+     */
     public function getRemarks()
     {
         return $this->remarks;
     }
 
     /**
-    * @param TEXT_LONG $remarks
-    *
-    */
+     * @param TEXT_LONG $remarks
+     *
+     */
     public function setRemarks($remarks)
     {
         $this->remarks = $remarks;
@@ -221,17 +231,17 @@ class Soa extends AbstractEntity
 
 
     /**
-    * @return TEXT_LONG
-    */
+     * @return TEXT_LONG
+     */
     public function getEvidences()
     {
         return $this->evidences;
     }
 
     /**
-    * @param TEXT_LONG $evidences
-    *
-    */
+     * @param TEXT_LONG $evidences
+     *
+     */
     public function setEvidences($evidences)
     {
         $this->evidences = $evidences;
@@ -239,17 +249,17 @@ class Soa extends AbstractEntity
 
 
     /**
-    * @return TEXT_LONG
-    */
+     * @return TEXT_LONG
+     */
     public function getActions()
     {
         return $this->actions;
     }
 
     /**
-    * @param TEXT_LONG $actions
-    *
-    */
+     * @param TEXT_LONG $actions
+     *
+     */
     public function setActions($actions)
     {
         $this->actions = $actions;
@@ -262,17 +272,17 @@ class Soa extends AbstractEntity
 
 
     /**
-    * @return integer
-    */
+     * @return integer
+     */
     public function getCompliance()
     {
         return $this->compliance;
     }
 
     /**
-    * @param integer $compliance
-    *
-    */
+     * @param integer $compliance
+     *
+     */
     public function setCompliance($compliance)
     {
         $this->compliance = $compliance;
@@ -280,8 +290,8 @@ class Soa extends AbstractEntity
 
 
     /**
-    * @return boolean
-    */
+     * @return boolean
+     */
     public function isIsDeleted()
     {
         return $this->isDeleted;
@@ -289,34 +299,34 @@ class Soa extends AbstractEntity
 
 
     /**
-    * @return int
-    */
+     * @return int
+     */
     public function getEx()
     {
         return $this->EX;
     }
 
     /**
-    * @param int $EX
-    *
-    */
+     * @param int $EX
+     *
+     */
     public function setEx($EX)
     {
         $this->EX = $EX;
     }
 
     /**
-    * @return int
-    */
+     * @return int
+     */
     public function getLr()
     {
         return $this->LR;
     }
 
     /**
-    * @param int $LR
-    *
-    */
+     * @param int $LR
+     *
+     */
     public function setLr($LR)
     {
         $this->LR = $LR;
@@ -324,51 +334,51 @@ class Soa extends AbstractEntity
 
 
     /**
-    * @return int
-    */
+     * @return int
+     */
     public function getCo()
     {
         return $this->CO;
     }
 
     /**
-    * @param int $CO
-    *
-    */
+     * @param int $CO
+     *
+     */
     public function setCo($CO)
     {
         $this->CO = $CO;
     }
 
     /**
-    * @return int
-    */
+     * @return int
+     */
     public function getBr()
     {
         return $this->BR;
     }
 
     /**
-    * @param int $BR
-    *
-    */
+     * @param int $BR
+     *
+     */
     public function setBr($BR)
     {
         $this->BR = $BR;
     }
 
     /**
-    * @return int
-    */
+     * @return int
+     */
     public function getBp()
     {
         return $this->BP;
     }
 
     /**
-    * @param int $BP
-    *
-    */
+     * @param int $BP
+     *
+     */
     public function setBp($BP)
     {
         $this->BP = $BP;
@@ -376,17 +386,17 @@ class Soa extends AbstractEntity
 
 
     /**
-    * @return int
-    */
+     * @return int
+     */
     public function getRra()
     {
         return $this->RRA;
     }
 
     /**
-    * @param int $RRA
-    *
-    */
+     * @param int $RRA
+     *
+     */
     public function setRra($RRA)
     {
         $this->RRA = $RRA;
