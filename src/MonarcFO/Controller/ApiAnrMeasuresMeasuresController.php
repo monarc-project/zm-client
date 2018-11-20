cf76bf43bc3a3f772ed75cbe0a519f853019bb80
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-11-20 09:18:59 +0100

    Like in the back office load the anr, father and child dependencies.

--- src/MonarcFO/Controller/ApiAnrMeasuresMeasuresController.php
+++ src/MonarcFO/Controller/ApiAnrMeasuresMeasuresController.php
@@ -17,5 +17,44 @@ use Zend\View\Model\JsonModel;
 class ApiAnrMeasuresMeasuresController extends ApiAnrAbstractController
 {
     protected $name = 'measuresmeasures';
-    protected $dependencies = [];
+    protected $dependencies = ['anr', 'father', 'child'];
+
+    /**
+     * @inheritdoc
+     */
+    public function getList()
+    {
+        $page = $this->params()->fromQuery('page');
+        $limit = $this->params()->fromQuery('limit');
+        $order = $this->params()->fromQuery('order');
+        $filter = $this->params()->fromQuery('filter');
+      //  $status = $this->params()->fromQuery('status');
+        $fatherId = $this->params()->fromQuery('fatherId');
+        $childId = $this->params()->fromQuery('childId');
+        $filterAnd = [];
+        // if (is_null($status)) {
+        //     $status = 1;
+        // }
+        // $filterAnd = ($status == "all") ? null : ['status' => (int) $status] ;
+        if ($fatherId) {
+          $filterAnd['father'] = (int) $fatherId;
+        }
+        if ($childId) {
+          $filterAnd['child'] = (int) $childId;
+        }
+
+        $service = $this->getService();
+
+        $entities = $service->getList($page, $limit, $order, $filter, $filterAnd);
+        if (count($this->dependencies)) {
+            foreach ($entities as $key => $entity) {
+                $this->formatDependencies($entities[$key], $this->dependencies);
+            }
+        }
+
+        return new JsonModel(array(
+            'count' => $service->getFilteredCount($filter, $filterAnd),
+            $this->name => $entities
+        ));
+    }
 }
