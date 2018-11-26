a14c6f028bce4ec1e4b6fbd017054e34c3507a7b
Juan Rocha <juan.rocha@securitymadein.lu>
2018-11-26 16:51:52 +0100

    Add getList() to Category API

--- src/MonarcFO/Controller/ApiSoaCategoryController.php
+++ src/MonarcFO/Controller/ApiSoaCategoryController.php
@@ -6,6 +6,9 @@
  */
 
 namespace MonarcFO\Controller;
+
+use Zend\View\Model\JsonModel;
+
 /**
  * Api ANR Categories Controller
  *
@@ -15,6 +18,47 @@ namespace MonarcFO\Controller;
 class ApiSoaCategoryController extends ApiAnrAbstractController
 {
     protected $name = 'categories';
-    protected $dependencies = ['anr'];
+    protected $dependencies = ['anr', 'referential'];
+
+    /**
+     * @inheritdoc
+     */
+    public function getList()
+    {
+        $anrId = (int)$this->params()->fromRoute('anrid');
+        if (empty($anrId)) {
+            throw new \MonarcCore\Exception\Exception('Anr id missing', 412);
+        }
+        $page = $this->params()->fromQuery('page');
+        $limit = $this->params()->fromQuery('limit');
+        $order = $this->params()->fromQuery('order');
+        $filter = $this->params()->fromQuery('filter');
+        $status = $this->params()->fromQuery('status');
+        $referential = $this->params()->fromQuery('referential');
+
+        $filterAnd = [];
+        if (is_null($status)) {
+            $status = 1;
+        }
+        $filterAnd = ($status == "all") ? null : ['status' => (int) $status] ;
+        $filterAnd = ['anr' => $anrId];
+        if ($referential) {
+          $filterAnd['referential'] = (array)$referential;
+        }
+
+        $service = $this->getService();
+
+        $entities = $service->getList($page, $limit, $order, $filter, $filterAnd);
+        if (count($this->dependencies)) {
+            foreach ($entities as $key => $entity) {
+                $this->formatDependencies($entities[$key], $this->dependencies);
+            }
+        }
+
+        return new JsonModel(array(
+            'count' => $service->getFilteredCount($filter, $filterAnd),
+            $this->name => $entities
+        ));
+    }
 
 }
