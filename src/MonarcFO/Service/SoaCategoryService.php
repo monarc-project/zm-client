c45f2be189df076359734fd864fe46f9ae878839
Juan Rocha <juan.rocha@securitymadein.lu>
2018-12-03 15:53:27 +0100

    Clean useless function

--- src/MonarcFO/Service/SoaCategoryService.php
+++ src/MonarcFO/Service/SoaCategoryService.php
@@ -31,32 +31,4 @@ class SoaCategoryService extends \MonarcCore\Service\AbstractService
         $this->filterPatchFields($data);
         parent::patch($id, $data);
     }
-
-    /**
-    * @inheritdoc
-    */
-    public function getList($page = 1, $limit = 25, $order = null, $filter = null, $filterAnd = null)
-    {
-        $data = $this->get('table')->fetchAllFiltered(
-            array_keys($this->get('entity')->getJsonArray()),
-            1,
-            0,
-            $this->parseFrontendOrder($order),
-            $this->parseFrontendFilter($filter, $this->filterColumns),
-            $filterAnd
-        );
-        if ($order == "code" || $order == "-code") {
-            $desc = ($order == "-code");
-            usort($data, function ($a, $b) use ($re, $desc) {
-                $a_match = (intval($a['code']));
-                $b_match = (intval($b['code']));
-                if ($a_match && $b_match) {
-                    return $desc ? (intval($b['code']) - intval($a['code'])) : (intval($a['code']) - intval($b['code']));
-                }  else {
-                    return $desc ? strcmp($b_match, $a_match) : strcmp($a_match, $b_match);
-                }
-            });
-        }
-        return array_slice($data, ($page - 1) * $limit, $limit, false);
-    }
 }
