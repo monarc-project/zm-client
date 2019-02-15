8b61ccf23e7fc9cb9749dbdc02c8c4dbe96b197f
Jerome Lombardi <jerome.lombardi@cases.lu>
2019-02-15 10:46:49 +0100

    Factorize the createlinkedamvs function

--- src/MonarcFO/Controller/ApiAnrAmvsController.php
+++ src/MonarcFO/Controller/ApiAnrAmvsController.php
@@ -136,9 +136,8 @@ class ApiAnrAmvsController extends ApiAnrAbstractController
 
     public function patchList($data)
     {
-      $anrId = (int)$this->params()->fromRoute('anrid');
       $service = $this->getService();
-
+      $data['toReferential'] = $this->addAnrId($data['toReferential']);
       $service->createLinkedAmvs($data['fromReferential'],$data['toReferential'],$anrId);
 
       return new JsonModel([
