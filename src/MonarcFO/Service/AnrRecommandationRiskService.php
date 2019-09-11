c6fdb47057f932d8c06f69842a8ab01c255f01ea
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-09-11 12:49:21 +0200

    fix: [service ]cast the value of recommendation's position when detaching a recommendation in AnrRecommandationRiskService

--- src/MonarcFO/Service/AnrRecommandationRiskService.php
+++ src/MonarcFO/Service/AnrRecommandationRiskService.php
@@ -880,11 +880,11 @@ class AnrRecommandationRiskService extends \MonarcCore\Service\AbstractService
             $table->delete($recommandationRisk->id);
         }
 
-        // Update brother's recommandation position if necessary
+        // Update brother's recommendation position if necessary
         $bros = current($table->getEntityByFields(['anr' => $idAnr,'recommandation'=>$idReco, 'id'=>['op'=>'!=', 'value'=>$id]]));
         if(empty($bros)){ // is last recorisk
             $reco = $this->get('recommandationTable')->getEntity($idReco);
-            $recos = $this->get('recommandationTable')->getEntityByFields(['anr'=>$reco->get('anr')->get('id'), 'position' => ['op' => '>', 'value'=>$reco->get('position')]],['position'=>'ASC']);
+            $recos = $this->get('recommandationTable')->getEntityByFields(['anr'=>$reco->get('anr')->get('id'), 'position' => ['op' => '>', 'value'=>(int)$reco->get('position')]],['position'=>'ASC']);
             foreach($recos as $r){
                 $r->set('position',$r->get('position')-1);
                 $this->get('recommandationTable')->save($r,false);
