3b00b273c8c1a9f5aca4c252dcc7ea239e1a374f
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2019-12-11 09:28:55 +0100

    Fixed the migration.

--- migrations/db/20191210165401_fix_recommendations_nullable_position.php
+++ migrations/db/20191210165401_fix_recommendations_nullable_position.php
@@ -7,23 +7,30 @@ class FixRecommendationsNullablePosition extends AbstractMigration
     public function change()
     {
         $anrs = $this->query('SELECT `id` from `anrs`')->fetchAll();
+        if (empty($anrs)) {
+            return;
+        }
         foreach ($anrs as $anr) {
-            $stmtMaxPosition = $this->query('SELECT MAX(`position`) AS max_position from `recommandations` where `anr_id` = :anrId AND `position` IS NOT NULL');
-            $stmtMaxPosition->bindParam(':anrId', $anr['id'], PDO::PARAM_INT);
-            $maxPositionResult = $stmtMaxPosition->execute();
+            $stmtMaxPosition = $this->query(
+                'SELECT MAX(`position`) AS max_position from `recommandations` where `anr_id` = ' . $anr['id'] . ' AND `position` IS NOT NULL'
+            );
+            $maxPositionResult = $stmtMaxPosition->fetchAll();
             $maxPosition = 0;
             if (!empty($maxPositionResult) && !empty($maxPositionResult[0]['max_position'])) {
                 $maxPosition = $maxPositionResult[0]['max_position'];
             }
-            $recommendationsToUpdate = $this->query('SELECT `id` from `recommandations` where `anr_id` = :anrId AND `position` IS NULL ORDER BY `created_at`');
-            $recommendationsToUpdate->bindParam(':anrId', $anr['id'], PDO::PARAM_INT);
-            $recommendationsToUpdateResult = $recommendationsToUpdate->execute();
+            $recommendationsToUpdate = $this->query(
+                'SELECT `uuid` from `recommandations` where `anr_id` = ' . $anr['id'] . ' AND `position` IS NULL ORDER BY `created_at`'
+            );
+            $recommendationsToUpdateResult = $recommendationsToUpdate->fetchAll();
+            if (empty($recommendationsToUpdateResult)) {
+                continue;
+            }
             foreach ($recommendationsToUpdateResult as $recommendation) {
-                $updateStmt = $this->query('UPDATE `recommandations` SET position = :position WHERE uuid = :uuid AND anr_id = :anId');
-                $updateStmt->bindParam(':uuid', $recommendation['uuid'], PDO::PARAM_INT);
-                $updateStmt->bindParam(':anrId', $anr['id'], PDO::PARAM_INT);
-                $updateStmt->bindParam(':position', ++$maxPosition, PDO::PARAM_INT);
-                $updateStmt->execute();
+                $this->execute(
+                    'UPDATE `recommandations` SET `position` = ' . ++$maxPosition
+                    . ' WHERE `uuid` = "' . $recommendation['uuid'] .  '" AND `anr_id` = ' . $anr['id']
+                );
             }
         }
     }
