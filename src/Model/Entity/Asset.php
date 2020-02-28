6d1150f413651b1344bd52ec076c0df6431f44ce
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2020-02-28 12:19:44 +0100

    Reverted back the entityes fields to fix the dependency initialization issues.

--- src/Model/Entity/Asset.php
+++ src/Model/Entity/Asset.php
@@ -8,14 +8,35 @@
 namespace Monarc\FrontOffice\Model\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Monarc\Core\Model\Entity\AnrSuperClass;
 use Monarc\Core\Model\Entity\AssetSuperClass;
 
 /**
  * Asset
  *
- * @ORM\Table(name="assets")
+ * @ORM\Table(name="assets", indexes={
+ *      @ORM\Index(name="anr_id", columns={"anr_id","code"}),
+ *      @ORM\Index(name="anr_id2", columns={"anr_id"})
+ * })
  * @ORM\Entity
  */
 class Asset extends AssetSuperClass
 {
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="uuid", type="uuid", nullable=false)
+     * @ORM\Id
+     */
+    protected $uuid;
+
+    /**
+     * @var AnrSuperClass
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="Anr", fetch="EAGER")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="anr_id", referencedColumnName="id", nullable=true)
+     * })
+     */
+    protected $anr;
 }
