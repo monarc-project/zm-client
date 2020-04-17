f29a0e09176149e474a0e4a0b7842290e740e92b
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-04-17 11:48:50 +0200

    new: [stats] added new command which will collect, aggregate and store stats.

---
+++ src/Command/CollectStats.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Monarc\FrontOffice\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Monarc\FrontOffice\Model\Entity\StatsAnr;
+use Monarc\FrontOffice\Model\Entity\Anr;
+
+class CollectStatsCommand extends Command
+{
+    protected static $defaultName = 'monarc:collect-stats';
+
+    /** @var StatsAnrTable */
+    private $statsAnrTable;
+
+    public function __construct(UserTable $statsAnrTable)
+    {
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        return 0;
+    }
+}
