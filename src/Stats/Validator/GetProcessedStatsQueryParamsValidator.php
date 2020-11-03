70f6c1472a31a1b28eb559b51191e91fa1c8f25f
Juan Rocha <juan.rocha@securitymadein.lu>
2020-11-03 14:09:53 +0100

    Fixed validator for date inputs

--- src/Stats/Validator/GetProcessedStatsQueryParamsValidator.php
+++ src/Stats/Validator/GetProcessedStatsQueryParamsValidator.php
@@ -87,18 +87,37 @@ class GetProcessedStatsQueryParamsValidator extends AbstractMonarcInputValidator
                     [
                         'name' => Date::class,
                     ],
+                    [
+                        'name' => Callback::class,
+                        'options' => [
+                            'callback' => [$this, 'validateDateFrom'],
+                        ],
+                    ],
+                ],
+            ],
+            [
+                'name' => 'dateTo',
+                'required' => false,
+                'validators' => [
+                    [
+                        'name' => Date::class,
+                    ],
+                    [
+                        'name' => Callback::class,
+                        'options' => [
+                            'callback' => [$this, 'validateDateTo'],
+                        ],
+                    ],
                 ],
             ],
         ];
     }
-    
+
     public function validateDateFrom($value, array $context = []): bool
     {
-        // if (empty($value)) {
-        //     return true;
-        // }
-        
-        file_put_contents('php://stderr', print_r($value , TRUE).PHP_EOL);
+        if (empty($value)) {
+            return true;
+        }
 
         $currentDate = new DateTime();
         if ($value > $currentDate->format('Y-m-d')) {
@@ -112,6 +131,32 @@ class GetProcessedStatsQueryParamsValidator extends AbstractMonarcInputValidator
         if (!empty($context['dateTo']) && $value > $context['dateTo']) {
             $this->inputFilter->getInputs()['dateFrom']->setErrorMessage(
                 '"dateFrom" should be lower or equal to "dateTo".'
+            );
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function validateDateTo($value, array $context = []): bool
+    {
+        if (empty($value)) {
+            return true;
+        }
+
+        $currentDate = new DateTime();
+        if ($value > $currentDate->format('Y-m-d')) {
+            $this->inputFilter->getInputs()['dateTo']->setErrorMessage(
+                '"dateTo" should be lower or equal to current date.'
+            );
+
+            return false;
+        }
+
+        if (!empty($context['dateFrom']) && $value < $context['dateFrom']) {
+            $this->inputFilter->getInputs()['dateTo']->setErrorMessage(
+                '"dateTo" should be bigger or equal to "dateFrom".'
             );
 
             return false;
