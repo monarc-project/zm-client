53560a575559f5044b75b4a7ff4d8c2b3ab89c7c
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2020-11-09 15:39:17 +0100

    Removed the inprut filter.

--- src/Stats/Validator/GetProcessedStatsQueryParamsValidator.php
+++ src/Stats/Validator/GetProcessedStatsQueryParamsValidator.php
@@ -69,7 +69,6 @@ class GetProcessedStatsQueryParamsValidator extends AbstractMonarcInputValidator
             [
                 'name' => 'processor_params',
                 'required' => false,
-                'type' => ArrayInput::class,
             ],
             [
                 'name' => 'nbdays',
