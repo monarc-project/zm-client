848fd8356946c66a5879518b208115b706e29a7f
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2021-06-16 15:47:25 +0200

    Feature/353 Fixed the index column name `instance_risk_op_id`.

--- src/Model/Entity/OperationalInstanceRiskScale.php
+++ src/Model/Entity/OperationalInstanceRiskScale.php
@@ -8,7 +8,7 @@ use Monarc\Core\Model\Entity\OperationalInstanceRiskScaleSuperClass;
 /**
  * @ORM\Table(name="operational_instance_risks_scales", indexes={
  *      @ORM\Index(name="op_ins_risk_scales_anr_id_indx", columns={"anr_id"}),
- *      @ORM\Index(name="op_ins_risk_scales_op_ins_risk_id_indx", columns={"operational_instance_risk_id"}),
+ *      @ORM\Index(name="op_ins_risk_scales_instance_risk_op_id_indx", columns={"instance_risk_op_id"}),
  *      @ORM\Index(name="op_ins_risk_scales_op_risk_scale_id_indx", columns={"operational_risk_scale_id"})
  * })
  * @ORM\Entity
