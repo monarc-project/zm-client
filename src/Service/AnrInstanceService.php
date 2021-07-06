22ebff14cd1bedb06eaf3251c5a2fed61aaf2429
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2021-07-06 16:00:54 +0200

    Feature/353 Added the context to the get response.

--- src/Service/AnrInstanceService.php
+++ src/Service/AnrInstanceService.php
@@ -48,4 +48,62 @@ class AnrInstanceService extends InstanceService
             $this->resetRecommendationsPositions($anr, $recommendationsToResetPositions);
         }
     }
+
+    protected function getExportedRecommendations(
+        bool $withEval,
+        bool $withRecommendations,
+        bool $withUnlinkedRecommendations,
+        array $riskIds
+    ): array {
+        if ($withEval && $withRecommendations) {
+            $recsSetsObj = [
+                'uuid' => 'uuid',
+                'label1' => 'label1',
+                'label2' => 'label2',
+                'label3' => 'label3',
+                'label4' => 'label4',
+            ];
+
+            $return['recSets'] = [];
+            $recommandationsSets = $this->get('recommandationSetTable')->getEntityByFields(['anr' => $instance->get('anr')->get('id')]);
+            foreach ($recommandationsSets as $recommandationSet) {
+                $return['recSets'][$recommandationSet->getUuid()] = $recommandationSet->getJsonArray($recsSetsObj);
+            }
+        }
+
+        $recoIds = [];
+        if ($withEval && $withRecommendations && !empty($riskIds) && $this->get('recommandationRiskTable')) {
+            $recosObj = [
+                'uuid' => 'uuid',
+                'recommandationSet' => 'recommandationSet',
+                'code' => 'code',
+                'description' => 'description',
+                'importance' => 'importance',
+                'comment' => 'comment',
+                'status' => 'status',
+                'responsable' => 'responsable',
+                'duedate' => 'duedate',
+                'counterTreated' => 'counterTreated',
+            ];
+            $return['recos'] = [];
+            if (!$withUnlinkedRecommendations) {
+                $return['recs'] = [];
+            }
+            $recoRisk = $this->get('recommandationRiskTable')->getEntityByFields(['anr' => $instance->get('anr')->get('id'), 'instanceRisk' => $riskIds], ['id' => 'ASC']);
+            foreach ($recoRisk as $rr) {
+                if (!empty($rr)) {
+                    $recommendation = $rr->getRecommandation();
+                    $recommendationUuid = $recommendation->getUuid();
+                    $return['recos'][$rr->get('instanceRisk')->get('id')][$recommendationUuid] = $rr->get('recommandation')->getJsonArray($recosObj);
+                    $return['recos'][$rr->get('instanceRisk')->get('id')][$recommendationUuid]['recommandationSet'] = $rr->getRecommandation()->getRecommandationSet()->getUuid();
+                    $return['recos'][$rr->get('instanceRisk')->get('id')][$recommendationUuid]['commentAfter'] = $rr->get('commentAfter');
+                    if (!$withUnlinkedRecommendations && !isset($recoIds[$recommendationUuid])) {
+                        $return['recs'][$recommendationUuid] = $recommendation->getJsonArray($recosObj);
+                        $return['recs'][$recommendationUuid]['recommandationSet'] = $recommendation->getRecommandationSet()->getUuid();
+                    }
+                    $recoIds[$recommendationUuid] = $recommendationUuid;
+                }
+            }
+        }
+    }
 }
