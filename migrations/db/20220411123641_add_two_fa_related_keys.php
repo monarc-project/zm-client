b18b00134e5da960dbbd3a0ad9f409e84e724064
Cédric Bonhomme <cedric@cedricbonhomme.org>
2022-04-12 12:40:39 +0200

    chg: [2fA] added two_factor_enabled, secret_key and recovery_codes columns to the users table.

---
+++ migrations/db/20220411123641_add_two_fa_related_keys.php
@@ -0,0 +1,41 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddTwoFaRelatedKeys extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('two_factor_enabled', 'boolean', array('default' => false));
+        $table->addColumn('secret_key', 'string', array('null' => true, 'default' => '', 'after' => 'password'));
+        $table->addColumn('recovery_codes', 'string', array('null' => true, 'default' => '', 'after' => 'password'));
+        $table->update();
+
+    }
+}
