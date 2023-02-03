3c0957387ce181128589962ad1ba91df47d4b9ef
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2023-02-03 15:25:05 +0100

    Fixed the long objects' names generation issue and only add 'Imp #' within the same category.

--- src/Service/InstanceImportService.php
+++ src/Service/InstanceImportService.php
@@ -2314,8 +2314,8 @@ class InstanceImportService
         $instanceData = $data['instance'];
         $instance = (new Instance())
             ->setAnr($anr)
-            ->setLabels($instanceData)
-            ->setNames($instanceData)
+            ->setLabels($monarcObject->getLabels())
+            ->setNames($monarcObject->getNames())
             ->setDisponibility(!empty($instanceData['disponibility']) ? (float)$instanceData['disponibility'] : 0)
             ->setLevel($parentInstance === null ? Instance::LEVEL_ROOT : $instanceData['level'])
             ->setRoot($parentInstance === null ? null : ($parentInstance->getRoot() ?? $parentInstance))
